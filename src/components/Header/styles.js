d5483ff72a46b4d71f0e730b955fbeb919c26749
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-09 00:07:40 -0300

    Feat: Styled Header container

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
-  ${({ theme }) => css``}
+export const Container = styled.header`
+  ${({ theme }) => css`
+    max-width: 300px;
+    border: solid 1px red;
+    margin: 0 auto;
+    text-align: center;
+  `}
 `;
