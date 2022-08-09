aee646c97acfec86326fdd9f36fb923255973117
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-09 00:12:13 -0300

    Feat: Styled Section container

--- src/components/Section/styles.js
+++ src/components/Section/styles.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 import styled, { css } from 'styled-components';
 
-export const Container = styled.header`
-  ${({ theme }) => css``}
+export const Container = styled.section`
+  ${({ theme }) => css`
+    max-width: 300px;
+    margin: 0 auto;
+  `}
 `;
