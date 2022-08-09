3a43a0282d25887305a98674e29ce3f3387339c4
AlexMarquesAlves <atanaelleonardo@gmail.com>
2022-08-08 21:55:10 -0300

    Feat: Reset CSS

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -1,3 +1,17 @@
 import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle``;
+export const GlobalStyles = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+
+    body{
+    background-color: #ffedd5;
+
+
+    ul{
+      list-style: none;
+    }
+  }
+  }
+`;
